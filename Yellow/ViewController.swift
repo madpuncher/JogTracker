3218fb11a206b642138466bd6054ae82abe2c296
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2021-11-18 12:48:38 +0300

    Initial Commit

---
+++ Yellow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yellow
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 18.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
